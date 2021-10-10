b784d017ad2217f2035a6284c232b38f608a9e8a
Rahul4325 <90451242+Rahul4325@users.noreply.github.com>
2021-10-10 09:51:18 +0530

    Update m2-t6-e4.cpp

--- m2-diving-into-c-programming/m2-code challenges - solution/m2-t6-e4.cpp
+++ m2-diving-into-c-programming/m2-code challenges - solution/m2-t6-e4.cpp
@@ -1,3 +1,10 @@
+/*Write a program to calculate the perimeter of a given rectangle.
+
+Take the length and breadth of the rectangle as user input.
+The length and breadth could be a decimal value entered by the user.
+Define a function that will accept the length and breadth of the rectangle as parameters and return the perimeter of that rectangle.
+Additionally, you can greet the user with a welcome message. To keep the main function clean, put the code to greet the user within a separate function.
+*/
 #include <stdio.h>
 
 /* Function Declarations. */
